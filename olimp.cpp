56633a72368e5036a871d10d598e56c4cba7be89
korolinakila <112556625+korolinakila@users.noreply.github.com>
2023-09-19 18:24:28 +0300

    Add files via upload

---
+++ olimp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector <int> mass;
+    int n=45, k=1, f=1, p, y;
+    mass.push_back(k);
+    mass.push_back(f);
+    for(int i=2; i<n; i++){
+        mass.push_back((mass[i-2]%10)+(mass[i-1]%10));
+    }
+    cin >> y;
+    cout << mass[y]%10;
+
+
+return 0;
+}
